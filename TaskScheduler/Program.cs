8c84c82d23db1cc6c2643741b8885defc8de22eb
Pawel Lysko <lyskopawel03@gmail.com>
2025-04-17 13:48:25 +0200

    add comment

--- TaskScheduler/Program.cs
+++ TaskScheduler/Program.cs
@@ -5,6 +5,7 @@ internal class Program
     static void Main(string[] args)
     {
         var taskService = new TaskService();
+        //seed initial data
         var task1 = new Task(
             "Work meeting",
             "",
