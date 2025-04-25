42fa340d70b59db45c95b626ac77f2f9f3b4ced7
Pawel Lysko <lyskopawel03@gmail.com>
2025-04-25 21:20:44 +0200

    Implement storage service to save and load json files

---
+++ ConsoleCalendar.App/Abstract/IStorageService.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleCalendar.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalendar.App.Abstract
+{
+    public interface IStorageService<T> where T : BaseEnitity
+    {
+        public List<T> Load();
+        public void Save(List<T> data);
+
+    }
+}
---
+++ ConsoleCalendar.App/Concrete/CalendarEventJsonStorageService.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleCalendar.App.Abstract;
+using Newtonsoft.Json;
+
+namespace ConsoleCalendar.App.Concrete
+{
+    public class CalendarEventJsonStorageService : IStorageService<CalendarEvent>
+    {
+        private readonly string filePath;
+
+        public CalendarEventJsonStorageService(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public List<CalendarEvent> Load()
+        {
+            if (!File.Exists(filePath))
+                return new List<CalendarEvent>();
+
+            using var reader = new StreamReader(filePath);
+            var json = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<List<CalendarEvent>>(json) ?? new List<CalendarEvent>();
+        }
+
+        public void Save(List<CalendarEvent> data)
+        {
+            using var writer = new StreamWriter(filePath);
+            var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            writer.Write(json);
+        }
+    }
+}
